123a10f60a5e2b170fe50db6f48d2fc3e2e5e6d6
DariaJurr <zhuromskaya_ds@mail.ru>
2023-02-08 16:38:17 +0700

    fix: close popup menu

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,28 @@ const menuBtn = document.querySelector('.header__menu');
 const menuPopup = document.querySelector('.header__popup');
 const fadeElems = document.querySelectorAll(".has-fade");
 
-const openMenuAnimate = () => {
+const closeMenuAnimate = () => {
     fadeElems.forEach((elem) =>{
         elem.classList.remove('fade-in');
         elem.classList.add('fade-out');               
     });    
+    menuBtn.classList.remove('active');
 };
 
-const closeMenuAnimate = () => {
+const openMenuAnimate = () => {
     fadeElems.forEach((elem) =>{
         elem.classList.remove('fade-out'); 
         elem.classList.add('fade-in');            
     });
+    menuBtn.classList.add('active');
 };
 
 menuBtn.addEventListener('click', e => {
-    menuBtn.classList.contains('active') ? openMenuAnimate() : closeMenuAnimate(); 
-    menuBtn.classList.toggle('active');
-    
+    menuBtn.classList.contains('active') ? closeMenuAnimate() : openMenuAnimate();     
 });
+
+document.addEventListener('click', e => {
+    if (e.target.classList.contains('header__popup-link') || e.target.classList.contains('overlay')) {
+        closeMenuAnimate();
+    }
+})
